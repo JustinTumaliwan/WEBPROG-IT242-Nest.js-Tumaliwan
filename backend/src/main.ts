bca312f651cc1489a6ec245ed3031488ebafbaa5
JustinTumaliwan <jgtumaliwan@student.apc.edu.ph>
2026-02-22 00:37:34 +0800

    changed the cors ting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // This opens the gates completely
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
-  // CRITICAL FIX: This allows Vercel to assign its own port
+  // This default setting safely opens the gates to all frontends
+  app.enableCors();
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
