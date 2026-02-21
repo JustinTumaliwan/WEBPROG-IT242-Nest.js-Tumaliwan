43f312902316415600e8ef12bd68e83d5ca72bd2
JustinTumaliwan <jgtumaliwan@student.apc.edu.ph>
2026-02-22 01:52:34 +0800

    added stuff

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // A blank command allows ALL origins, including Preview URLs
-  app.enableCors();
+  // Explicitly open the gates to ALL origins and methods
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
